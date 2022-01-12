65d44cbfbb59d269073437a4b300a2d02dbb1b17
daeunLee <eleeje97@gmail.com>
2022-01-12 19:43:31 +0900

    project build

---
+++ src/main/java/org/m2c/ComeSeeStudyApplication.java
@@ -0,0 +1,11 @@
+package org.m2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeSeeStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComeSeeStudyApplication.class, args);
+    }
+}
