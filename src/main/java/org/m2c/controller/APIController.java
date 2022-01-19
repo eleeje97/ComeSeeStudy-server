45c05b25d006f36bff349abe6d9e22fc3207d545
daeunLee <eleeje97@gmail.com>
2022-01-19 19:48:53 +0900

    test api, DB connection

--- src/main/java/org/m2c/controller/APIController.java
+++ src/main/java/org/m2c/controller/APIController.java
@@ -1,4 +1,23 @@
 package org.m2c.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.m2c.entity.*;
+import org.m2c.repo.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/css")
 public class APIController {
+    private final AttributeRepository attributeRepository;
+    private final AttrContentRepository attrContentRepository;
+    private final QuizRepository quizRepository;
+
+    @GetMapping("/test")
+    public List<AttributeEntity> findAllAttribute() {
+        return attributeRepository.findAll();
+    }
+
 }
